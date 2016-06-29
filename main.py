c9602d502f19ebaf6ab3468fae3ac6dc75b1cdbb
oyvindhg <oyvindhg@gmail.com>
2016-06-29 10:32:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame.camera
 import time
 import collections
+import matplotlib.pyplot as plt
 
-from camera import startCamera, takePicture, dispPicture
+from camera import startCamera, takePicture, dispPicture, selSearch
 from caffe_functions import caffeSetup, classify, printClassPred
 
 caffe_init = caffeSetup(0, 'ogn')
@@ -10,16 +11,44 @@ labels, transformer, net = caffe_init
 
 cam = startCamera()
 
-while (1):
+b = 1
+while (b == 1):
+
+    tic = time.time()
 
     image = takePicture(cam)
-    dispPicture(image.disp)
+    #dispPicture(image.disp)
 
+    print 'Searching..'
+    candidates = selSearch(image.classify)
+    print candidates
+    elem = candidates.pop()
+    print elem
+
+    #cropped_im = image.classify.crop(elem)
+
+    #cropped_im.show()
+
+    toc = time.time()
+
+    print 'Searching took ', toc - tic, 'sec'
+
+    tic = time.time()
+
+    print 'Classifying..'
     prob = classify(image.classify, transformer, net)
 
-    n = 5
+    toc = time.time()
+
+    print 'Classifying took ', toc - tic, 'sec'
+
+    n = 10
     printClassPred(prob, labels, n)
 
-    time.sleep(1)
+    b = 0
+    #time.sleep(1)
+
+
+plt.show()
 
 cam.stop()
