ebf88e01615ffd82b9b11a0db8f000251ae63c6e
HelgeKnutsen <helgkn@stud.ntnu.no>
2016-07-06 14:24:51 +0200

    Add files via upload

--- list_n_label.py
+++ list_n_label.py
@@ -10,15 +10,18 @@ def save_n_label(path, savefolder, label, ratio):
 
     n = len(onlyfiles) # number of images total in folder
     num_test = n/(ratio+1) # number of images saved in test-file
-    j = 0 # current number of images in test
+    #j = 0 # current number of images in test
 
-    for i in xrange(n):  # iterate over file names
-        if (random.randint(0,ratio) < ratio or j >= num_test) and i-j >= num_test-j:
-            train.write(path + '/' + onlyfiles[i] + ' ' + label + '\n')
-        else:
-            test.write(path + '/' + onlyfiles[i] + ' ' + label + '\n')
-            j += 1
-
+    for i in xrange(n-num_test):
+        train.write(path + '/' + onlyfiles[i] + ' ' + label + '\n')
+    for j in xrange(num_test):
+        test.write(path + '/' + onlyfiles[i] + ' ' + label + '\n')
+    #for i in xrange(n):  # iterate over file names
+        #if (random.randint(0,ratio) < ratio or j >= num_test) and i >= num_test:
+            #train.write(path + '/' + onlyfiles[i] + ' ' + label + '\n')
+        #else:
+            #test.write(path + '/' + onlyfiles[i] + ' ' + label + '\n')
+            #j += 1
     train.close()  # close train-file
     test.close() # close test-file
 
@@ -26,10 +29,13 @@ def save_n_label(path, savefolder, label, ratio):
 
 
 caffe_root = '/home/helge/caffe/' # caffe_root
-path = caffe_root + 'data/test 2016.06.29/5m, cropped/2016-06-29 Mennesker 5m' # folder with image files
-savefolder = caffe_root + 'myData/' # folder where image names and labels are saved
+path = caffe_root + 'data/test 2016.06.29/10m, cropped/2016-06-29 Bakgrunn 10m' # folder with image files
+savefolder = caffe_root + 'myData/2016-06-29 10m/' # folder where image names and labels are saved
 
-ratio = 7 # ratio of images saved in train-file vs test-file i.e. ratio = i means 1 of i images are saved in test-file
-label = '0'
+ratio = 5 # ratio of images saved in train-file vs test-file i.e. ratio = i means 1 of i images are saved in test-file
+# Note the following label system is applied
+# label = '0' for person
+# label = '1' for background
+label = '1'
 print save_n_label(path, savefolder, label, ratio)
 
