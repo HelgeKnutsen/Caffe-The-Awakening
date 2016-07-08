5aeb5051ce07842e38b9658b2b5972c76431eef7
HelgeKnutsen <helgkn@stud.ntnu.no>
2016-07-08 09:22:59 +0200

    Add files via upload

--- caffe_functions.py
+++ caffe_functions.py
@@ -1,9 +1,15 @@
-
+import pygame.camera
+import pygame.surfarray
+import time
 import caffe
 import sys
 import numpy as np
 import collections
 
+
+mod_def = 'myData/net_modified.prototxt' # path to model definitions-file 'models/bvlc_reference_caffenet/deploy.prototxt'
+weights = 'savedWeights/2016-06-29 5m, weights/weights.pretrained.caffemodel' # path to weights-file, reference weights at 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
+label_path = 'myData/test_names.txt' # 'data/ilsvrc12/synset_words.txt'
 
 # Choose cpu or gpu mode
 def set_caffe_mode(gpu):
@@ -19,9 +25,9 @@ def set_caffe_root(user):
     return '/home/'+ user +'/caffe/'
 
 # Set the caffe-net from bvlc_reference_caffenet
-def set_caffe_net(caffe_root):
-    model_def = caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt'
-    model_weights = caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
+def set_caffe_net(caffe_root, mod_def = mod_def, weights = weights):
+    model_def = caffe_root + mod_def
+    model_weights = caffe_root + weights
 
     return caffe.Net(model_def,      # defines the structure of the model
                 model_weights,  # contains the trained weights
@@ -42,8 +48,8 @@ def transform_input(mean, net, name = 'data'):
     transformer.set_channel_swap(name, (2, 1, 0))  # swap channels from RGB to BGR
     return transformer
 
-def load_labels(caffe_root):
-    labels_file = caffe_root + 'data/ilsvrc12/synset_words.txt'
+def load_labels(caffe_root, label_path = label_path):
+    labels_file = caffe_root + label_path
     return np.loadtxt(labels_file, str, delimiter='\t')
 
 # Set up caffe with initial settings
@@ -59,9 +65,9 @@ def caffeSetup(gpu, user, name = 'data'):
     # set the size of the input (we can skip this if we're happy
     #  with the default; we can also change it later, e.g., for different batch sizes)
     net.blobs[name].reshape(50,        # batch size
-                              3,         # 3-channel (BGR) images
-                              227, 227)  # image size is 227x227
-    caffe_init = collections.namedtuple('caffe_init', ['labels','transformer', 'net'])
+                            3,         # 3-channel (BGR) images
+                            227, 227)  # image size is 227x227
+    caffe_init = collections.namedtuple('caffe_init', ['labels', 'transformer', 'net'])
     return caffe_init(labels, transformer, net)
 
 
@@ -72,16 +78,17 @@ def classify(image, transformer, net, name = 'data'):
     ### perform classification
     output = net.forward()
     # NB! Might want to change the return statement to include more than first image in the batch
-    return output['prob'][0]  # the output probability vector for the first image in the batch
+    return output['probs'][0]  # the output probability vector for the first image in the batch
 
 def printClassPred(output_prob, labels, m): # m is number of items
     print 'predicted class is:', output_prob.argmax()
     print 'output label:', labels[output_prob.argmax()]
     print '\n'
-    top_inds = output_prob.argsort()[::-1][:m]  # reverse sort and take five largest items
+    top_inds = output_prob.argsort()[::-1][:m]  # reverse sort and take m largest items
     print 'probabilities and labels:'
-    for i in range(0, m):
+    for i in range(m):
         print output_prob[top_inds[i]], labels[top_inds[i]]
     print '\n'
     return 0
+
 
